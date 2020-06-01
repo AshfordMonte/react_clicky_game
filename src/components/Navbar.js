fa2d721ca28c18c5ed694f6e00ff1f497e427059
AshfoMon <ashfordmonte@gmail.com>
2020-06-01 18:20:04 -0500

    Created navbar component with score tracking state elements, some additional styling needed. Added bootstrap to index.html

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,26 @@
 import React from "react";
+
+class Navbar extends React.Component {
+  state = {
+    currentScore: 0,
+    topScore: 0
+  };
+
+  render() {
+    return (
+        <nav className="navbar navbar-dark bg-secondary">
+          <a class="navbar-brand" href="/">
+            React Clicky Game
+            </a>
+          <span class="navbar-text">
+            Click any image to start!
+          </span>
+          <span class="navbar-text">
+            Score: {this.state.currentScore} | Top Score: {this.state.topScore}
+          </span>
+        </nav>
+    );
+  }
+}
+
+export default Navbar;
