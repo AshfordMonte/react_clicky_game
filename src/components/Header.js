b283519c85fa776198719c58d6b0d28959720162
AshfoMon <ashfordmonte@gmail.com>
2020-06-02 16:55:00 -0500

    Added instructional header below navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,19 @@
 import React from "react";
+import "./styles/header.css";
+
+class Header extends React.Component {
+
+  render() {
+    return (
+      <div className="jumbotron jumbotron-fluid bg-secondary">
+        <div className="container">
+          <h1 className="display-1 text-white center">Clicky Game!</h1>
+          <p className="lead text-white center">Click on an image to earn points, if you click any image
+          more than once, you lose!</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
